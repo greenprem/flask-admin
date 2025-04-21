5f2901b8d5af72a41b5a7e3b2eac178f93192521
greenprem <prem121105@gmail.com>
2025-04-21 15:32:25 +0530

    views fix

--- app/db.py
+++ app/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, create_engine
 from sqlalchemy.orm import sessionmaker
 from app.config import config
 
