eebaf0d7069c0256135a2129ab1173b48ed6804f
greenprem <prem121105@gmail.com>
2025-04-21 15:31:17 +0530

    views fix

--- app/db.py
+++ app/db.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from app.config import config
 
-engine = create_async_engine(config.sqla_engine, echo=True)
+engine = create_engine(config.sqla_engine, echo=True)
 
 SessionLocal = sessionmaker(
     bind=engine,
