b99b51d1a79e29d1ee72165f7ec3302ae0800838
greenprem <prem121105@gmail.com>
2025-04-26 21:07:48 +0530

    homeview

--- app/sqla/views.py
+++ app/sqla/views.py
@@ -406,5 +406,5 @@ class HomeView(CustomView):
     async def render(self, request: Request, templates: Jinja2Templates) -> Response:
 
         return templates.TemplateResponse(
-            "greenhouse_panel.html", {"request": request}
+            "home.html", {"request": request}
         )
